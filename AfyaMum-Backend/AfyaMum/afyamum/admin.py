7f4a101fb63731ffd850662ec143019c82a34149
Ronnie Leon <ronleon76@gmail.com>
2023-08-09 09:58:07 +0000

    Authentication

---
+++ AfyaMum-Backend/AfyaMum/afyamum/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Mother, Specialist, Appointment
+
+admin.site.register(Mother)
+admin.site.register(Specialist)
+admin.site.register(Appointment)
