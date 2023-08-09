cc23f587cb17243703e9448d7c138b02f06a51eb
Ronnie Leon <ronleon76@gmail.com>
2023-08-09 11:56:02 +0000

    feat:Appointment CRUD functionalities

--- AfyaMum-Backend/AfyaMum/afyamum/models.py
+++ AfyaMum-Backend/AfyaMum/afyamum/models.py
@@ -33,3 +33,12 @@ class Appointment(models.Model):
     end_time = models.TimeField()
     specialist = models.ForeignKey(Specialist, related_name='specialist', on_delete=models.SET_NULL, null=True)
     mother = models.ForeignKey(Mother, related_name='doctor', on_delete=models.SET_NULL, null=True)
+
+
+class AppointmentRequest(models.Model):
+    from_mother = models.ForeignKey(Mother, on_delete=models.CASCADE)
+    to_doctor = models.ForeignKey(Specialist, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_accepted = models.BooleanField(default=False)
+    def __str__(self):
+        return self.from_mother.first_name
--- AfyaMum-Backend/AfyaMum/afyamum/urls.py
+++ AfyaMum-Backend/AfyaMum/afyamum/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
          name='token_obtain_pair'),
     path('register-as-mum/', registerMother, name='register'),
     path('register-as-specialist/', registerSpecialist, name='register'),
+    path('appointments/', AppointmentList.as_view()),
+    path('appointments/<int:pk>/', AppointmentDetail.as_view()),
+    path('appointmentrequest/', AppointmentRequestDetail.as_view()),
+    path('appointmentrequest/<int:pk>/', AppointmentRequestDetail.as_view()),
 
 ]
